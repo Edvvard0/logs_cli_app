c3427f6c0683396c2f8de7c785541c0fd468de7c
Edvvard0 <edward.osipchukzzz@gmail.com>
2025-04-13 22:33:40 +0300

    refactor handlers.py

--- reports/handlers.py
+++ reports/handlers.py
@@ -1,18 +1,15 @@
 from reports.base import BaseReport
-from utils.table_models import HandlersColumn, HandlersTable
+from utils.log_types import RequestLog
+from tables.handlers_table import HandlersColumn, HandlersTable
 
 
 class HandlersReport(BaseReport):
     def __init__(
-            self,
-            total_requests: int,
-            table: HandlersTable,
-            totals_data: HandlersColumn
-            ):
+        self, total_requests: int, table: HandlersTable, totals_data: HandlersColumn
+    ):
         self.total_requests = total_requests
         self.table = table
         self.totals_data = totals_data
-
 
     def generate(self):
         print(f"Total requests: {self.total_requests}")
@@ -26,3 +23,30 @@ class HandlersReport(BaseReport):
         print(self.totals_data.convert_columns())
 
 
+    @classmethod
+    def create_report(cls, file_paths: list[str]):
+        table = HandlersTable()
+        logs = RequestLog.convert_log_to_class(file_paths)
+        totals = HandlersColumn()
+        total_requests = 0
+        handlers_data = {}
+
+        for log in logs:
+            if log.message:
+                endpoint = log.message.endpoint
+
+                if endpoint not in handlers_data:
+                    column = HandlersColumn(handler=endpoint)
+                    handlers_data[endpoint] = column
+
+                level = str(log.level)
+
+                column.level_increment(level)
+                totals.level_increment(level)
+                total_requests += 1
+
+        table.columns = list(handlers_data.values())
+        handlers = HandlersReport(
+            total_requests=total_requests, table=table, totals_data=totals
+        )
+        return handlers
