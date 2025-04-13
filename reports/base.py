9c61bf629e060cf799486899b8b030531d8b6713
Edvvard0 <edward.osipchukzzz@gmail.com>
2025-04-13 15:52:19 +0300

    refactor base.py and log_parser.py

--- reports/base.py
+++ reports/base.py
@@ -1,2 +1,21 @@
-class BaseReport:
-    ...
+from abc import abstractmethod, ABC
+
+
+class BaseReport(ABC):
+    @abstractmethod
+    def generate(self) -> None:
+        """print a report"""
+        pass
+
+    @classmethod
+    @abstractmethod
+    def create_report(cls, lst_file_path: list[str]):
+        """
+        Сreates a report
+        Args:
+            file_paths (list[str]): List of file paths to the log files.
+
+        Returns:
+            report: (BaseReport)
+        """
+        pass
---
+++ utils/log_parser.py
@@ -0,0 +1,18 @@
+def parse_log_files(file_paths: list[str]) -> list[str]:
+    """
+    Reads log files and returns their contents as a list of strings.
+
+    Args:
+        file_paths (list[str]): List of file paths to the log files.
+
+    Returns:
+        list[str]: List of log entries.
+    """
+
+    logs = []
+    for file_path in file_paths:
+        with open(file_path, "r", encoding="utf-8") as f:
+            log = f.readlines()
+        logs.extend(log)
+
+    return logs
